d13731993243e945f02b4d64d125365a36b67ebc
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-09-12 18:34:26 +0500

    file add html css js

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+// const apiKey="98092c54b629e85a8a8adc138825a7b2"
+// const response =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKEY}`
+return (
+   <>
+   <div className="w-screen h-screen bg-red-700 flex justify-center flex-col">
+  <h1 className='text-center text-black font-bold mt-5 text-xl'>Weather App</h1>
+   <div className='h-[450px] w-96 rounded-xl mt-10 bg-orange-500 mx-auto'>
+
+
+   </div>
+  </div>
+   </>
+  )
+}
+
+export default App
