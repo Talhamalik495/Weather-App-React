fcf99db7fcf53065196356090c33f6e335289ad2
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-09-13 01:49:58 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,73 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
-// const apiKey="98092c54b629e85a8a8adc138825a7b2"
-// const response =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKEY}`
+  const [input,setInput]=useState("")
+  const [humaidity,setHumaidaty]=useState(62)
+  const [wind,setWind]=useState(6.17)
+  const [visibility,setVisibility]=useState(5)
+    console.log(input);
+    
+let search=async(input)=>{
+  try {
+    const APIKEY="75b4f55b56f54193c128d268511d6572"
+    const url=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=98092c54b629e85a8a8adc138825a7b2`)
+    const response= await url.json();
+
+console.log(response);
+  } catch (error) {
+    console.log(error); 
+  }
+}
+useEffect(()=>{
+  // search()
+  console.log("useeffect called");
+  search();
+  },[input])
 return (
    <>
-   <div className="w-screen h-screen bg-red-700 flex justify-center flex-col">
-  <h1 className='text-center text-black font-bold mt-5 text-xl'>Weather App</h1>
-   <div className='h-[450px] w-96 rounded-xl mt-10 bg-orange-500 mx-auto'>
+  <div className="w-screen h-screen flex justify-center flex-col">
+  <h1 className='text-center text-white font-bold mt-5 text-xl'>Weather App</h1>
+   <div className='h-[450px] w-[35%] rounded-xl mt-10 bg-white mx-auto flex justify-center flex-col align-middle items-center'>
 
-
-   </div>
+<div className='flex gap-3'> 
+  <input className='w-80 h-10 rounded-md bg-black' type="text"onChange={(e)=>setInput(e.target.value)} />
+<select onChange={(e)=>{
+setInput(e.target.value);
+}}>
+  <option value="Karachi">Karachi</option>
+  <option value="Tokyo">Tokyo</option>
+  <option value="New York">New York</option>
+  <option value="Paris">Paris</option>
+  <option value="London">London</option>
+  <option value="Shanghal">Shanghal</option>
+  <option value="Los Angleles">Los Angleles</option>
+  <option value="Beijing">Beijing</option>
+  <option value="Mumbai">Mumbai</option>
+  <option value="Moscow">Moscow</option>
+  <option value="Hong Kong">Hong Kong</option>
+  <option value="Dubai">Dubai</option>
+  <option value="Singapore">Singapore</option>
+  <option value="Bangkok">Bangkok</option>
+  <option value="Istambul">Istambul</option>
+  <option value="Sydney">Sydney</option>
+  <option value="Mexico City">Mexico City</option>
+  <option value="Berlin">Berlin</option>
+</select>
+</div>
+<div>
+  <div>
+    <img src="" alt="" onChange={()=>"ddd"} />
   </div>
+<div className='flex items-end gap-10 mt-40'>
+
+<p className='w-28 h-28 bg-black text-white rounded-md'>{wind}</p>
+  <p className='w-28 h-28 bg-black text-white rounded-md'>{humaidity}</p>
+  <p className='w-28 h-28 bg-black text-white rounded-md'>{visibility}</p>
+
+</div>
+</div>
+   </div>
+  </div> 
    </>
   )
 }
